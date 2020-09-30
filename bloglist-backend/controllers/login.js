6290c6bab647446886c1d57c8676cec7b7d465ef
Jessica Isoton <jessicasampaio195@gmail.com>
2020-09-30 14:30:30 +0200

    7.19 Style, step 1

--- bloglist-backend/controllers/login.js
+++ bloglist-backend/controllers/login.js
@@ -11,8 +11,6 @@ loginRouter.post("/", async (request, response) => {
     user === null
       ? false
       : await bcrypt.compare(body.password, user.passwordHash);
-
-  console.log(passwordCorrect);
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
       error: "invalid username or password",
